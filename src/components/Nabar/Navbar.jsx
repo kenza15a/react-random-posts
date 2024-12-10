d3934938a5e8a5542ee315665b57b52ba5968f72
Kenza Filali <filali1991.univ@gmail.com>
2024-12-10 16:47:25 +0100

    added logo and animation correced navbar links

--- src/components/Nabar/Navbar.jsx
+++ src/components/Nabar/Navbar.jsx
@@ -3,16 +3,17 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav className=" py-1 bg-[#9333EA] shadow-md text-white flex flex-row justify-around  md:justify-between gap-2  px-2 md:px-4 ">
+    <nav className="  bg-[#9333EA] shadow-md text-white flex flex-row justify-around  md:justify-between gap-2  px-2 md:px-4 ">
       <a href="/" className="style-none cursor-pointer">
-       
-         
-         <img src="/assets/logo random posts blanc.png" alt="logo" className=" w-28 h-auto mx-auto"/>
-       
+        <img
+          src="/assets/logo random posts blanc.png"
+          alt="logo"
+          className=" w-28 h-auto mx-auto"
+        />
       </a>
       <ul className=" list-none flex flex-row gap-2 md:gap-4 items-center justify-center  text-white">
         <li>
-          <a href="#offres">Accueil</a>
+          <a href="/">Accueil</a>
         </li>
         <li>
           <a href="#posts">Actualités</a>
