ff324057bcd7bce22bd76f607bc9b5596371cf04
Kenza Filali <filali1991.univ@gmail.com>
2024-12-11 09:26:04 +0100

    animated-app/ footer in compoents folder

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../../components/Nabar/Navbar";
-import Footer from "../../Footer/Footer";
+import Footer from "../../components/Footer/Footer";
 
 import PostsSection from "../../components/PostsSection/PostsSection";
 const HomePage = () => {
--- src/services/fetchRandomPosts.js
+++ src/services/fetchRandomPosts.js
@@ -1,13 +1,11 @@
 // postService.js
-
 export const fetchRandomPosts = async () => {
     try {
         const response = await fetch("https://jsonplaceholder.typicode.com/posts");
         const data = await response.json();
-        // Shuffle the array and take 5 random items
         return data.sort(() => 0.5 - Math.random()).slice(0, 5);
     } catch (error) {
         console.error("Error fetching posts:", error);
-        throw error; // Re-throw the error to handle it in the calling component
+        throw error;
     }
 };
